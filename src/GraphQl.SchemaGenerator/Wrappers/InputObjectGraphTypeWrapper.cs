62f8407e1593e5e4e6381feb479c5721e5f2ba88
Derek Holmes <derek.holmes@navitaire.com>
2016-09-21 10:18:50 -0600

    Revert "Work around for last known bug with graph ql."
    This reverts commit 2b1aba5a9f2cf66823651be1fcf0dccd4f0e5806.

--- src/GraphQl.SchemaGenerator/Wrappers/InputObjectGraphTypeWrapper.cs
+++ src/GraphQl.SchemaGenerator/Wrappers/InputObjectGraphTypeWrapper.cs
@@ -1,24 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using GraphQL.Types;
+﻿using GraphQL.Types;
 
 namespace GraphQL.SchemaGenerator.Wrappers
 {
-    /// <summary>
-    /// Note: the iobjectgraphtype is only used to get graph introspection to work. This is a work around until the bug is fixed.
-    /// see: https://github.com/graphql-dotnet/graphql-dotnet/issues/183
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    public class InputObjectGraphTypeWrapper<T> : InputObjectGraphType, IIgnore, IObjectGraphType
+    public class InputObjectGraphTypeWrapper<T> : InputObjectGraphType, IIgnore
     {
         public InputObjectGraphTypeWrapper()
         {
             ObjectGraphTypeBuilder.Build(this, typeof(T));
             Name = "Input_" + Name;
         }
-
-        public IEnumerable<Type> Interfaces { get; } = new List<Type>();
-
-        public Func<object, bool> IsTypeOf { get; set; }
     }
 }
